ec10131a4418545fe24c94ee4e578c1153a127f3
Haider-Mukhtar <haidermukhtarch405@gmail.com>
2025-10-30 16:06:37 +0500

    Settings Screen

--- app/(tabs)/(setting)/_layout.tsx
+++ app/(tabs)/(setting)/_layout.tsx
@@ -1,33 +1,36 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SettingLayout() {
   return (
-    <Stack
-      screenOptions={{
-        contentStyle: {
-          backgroundColor: "#FFFFFF"
-        }
-      }}
-    >
-      <Stack.Screen
-        name="setting"
-        options={{
-          title: "Settings",
-          headerLargeTitle: true,
-          headerLargeTitleShadowVisible: false,
-          headerBlurEffect: "systemUltraThinMaterial",
+    <SafeAreaView style={{flex: 1}}>
+      <Stack
+        screenOptions={{
+          contentStyle: {
+            backgroundColor: "#FFFFFF"
+          }
         }}
-      />
-      <Stack.Screen
-        name="details"
-        options={{
-          title: "Details",
-          headerLargeTitle: true,
-          headerLargeTitleShadowVisible: false,
-          headerBlurEffect: "systemUltraThinMaterial",
-          headerBackButtonDisplayMode: "minimal",
-        }}
-      />
-    </Stack>
+      >
+        <Stack.Screen
+          name="setting"
+          options={{
+            title: "Settings",
+            headerLargeTitle: true,
+            headerLargeTitleShadowVisible: false,
+            headerBlurEffect: "systemUltraThinMaterial",
+          }}
+        />
+        <Stack.Screen
+          name="details"
+          options={{
+            title: "Details",
+            headerLargeTitle: true,
+            headerLargeTitleShadowVisible: false,
+            headerBlurEffect: "systemUltraThinMaterial",
+            headerBackButtonDisplayMode: "minimal",
+          }}
+        />
+      </Stack>
+    </SafeAreaView>
   );
 }
--- app/(tabs)/(setting)/setting.tsx
+++ app/(tabs)/(setting)/setting.tsx
@@ -1,14 +1,19 @@
 import { router } from "expo-router";
-import { Text, TouchableWithoutFeedback, View } from "react-native";
+import { Switch, Text, TouchableWithoutFeedback, View } from "react-native";
 import { GlassView } from 'expo-glass-effect';
+import { useState } from "react";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Setting() {
+
+  const [isAirplaneMode, setIsAirplaneMode] = useState(false);
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        marginTop: 120,
+        paddingHorizontal: 16,
       }}
     >
       <TouchableWithoutFeedback
@@ -16,9 +21,12 @@ export default function Setting() {
       >
         <GlassView
           style={{
-            paddingVertical: 10,
+            paddingVertical: 16,
             paddingHorizontal: 20,
-            borderRadius: 50,
+            borderRadius: 12,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between"
           }}
           glassEffectStyle="clear"
           isInteractive
@@ -26,13 +34,44 @@ export default function Setting() {
         >
           <Text
             style={{
-              color: "#000000"
+              color: "#000000",
+              fontSize: 14,
+              fontWeight: 600,
             }}
           >
             Details
           </Text>
+          <Ionicons name="chevron-forward" size={24} color="black" />
         </GlassView>
       </TouchableWithoutFeedback>
+      <GlassView
+        style={{
+          paddingVertical: 16,
+          paddingHorizontal: 20,
+          borderRadius: 12,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginTop: 16,
+        }}
+        glassEffectStyle="clear"
+        isInteractive
+        tintColor="#99999990"
+      >
+        <Text
+          style={{
+            color: "#000000",
+            fontSize: 14,
+            fontWeight: 600,
+          }}
+        >
+          Notifications
+        </Text>
+        <Switch
+          value={isAirplaneMode}
+          onValueChange={setIsAirplaneMode}
+        />
+      </GlassView>
     </View>
   );
 }
